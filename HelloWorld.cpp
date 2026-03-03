ae6811ef12b5597aff6191906d74d982e5cf89cc
TheNightdemon <fujiarui061221>
2026-03-03 18:41:20 +0800

    change from HelloWorld2

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,6 +3,7 @@
 int main(int argc, char const *argv[]) {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "This is a simple C++ program." << std::endl;
+    std::cout << "This is a change from HelloWorld2" << std::endl;
     
     return 0;
 }
